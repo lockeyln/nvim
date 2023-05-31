72f40189402e2f3cb784a10931f09f2fa3e92516
lockeyln <2815657324@qq.com>
2023-05-31 20:41:23 +0800

    motify pyft

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -2,33 +2,3 @@ vim.opt_local.expandtab = true
 vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
 vim.opt_local.softtabstop = 4
-
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "python",
-    callback = function()
-        vim.api.nvim_buf_set_keymap(
-            0,
-            "n",
-            "<F4>",
-            -- "<cmd>!python %<CR>",
-            ":w<CR>:split<CR>:te time python3 %<CR>i",
-            { silent = true, noremap = true }
-        )
-    end,
-})
-
--- 自动创建python头部信息
-vim.cmd([[
-autocmd BufNewFile *.py,*.tex exec ":call SetTitle()"
-map <F1> :call SetTitle()<CR>
-func! SetTitle()
-    if &filetype == 'python'
-        call setline(1,"#!/usr/bin python3")
-        call append(line("."),"# -*- coding:UTF-8 -*-")
-        call append(line(".")+1, "# File Name: ".expand("%"))
-        call append(line(".")+2, "# Author: xssaw 🐬")
-        call append(line(".")+3, "# Created Time: ".strftime("%c"))
-    endif
-    normal Go
-endfunc
-]])
